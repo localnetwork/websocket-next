cc890374e05f057322654fe1989a2b25340f25d5
Diome Nike <diomenikep@gmail.com>
2024-04-17 20:05:05 +0800

    chore: initial commit

---
+++ pages/api/socket.js
@@ -0,0 +1,32 @@
+import { Server } from "socket.io";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  if (!res.socket.server.io) {
+    const httpServer = res.socket.server;
+    const io = new Server(httpServer, {
+      /* options */
+    });
+    httpServer.io = io;
+
+    io.on("connection", (socket) => {
+      console.log("A user connected");
+
+      socket.on("message", (message) => {
+        console.log("message: " + message);
+        io.emit("chat message", message);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("User disconnected");
+      });
+    });
+  }
+
+  res.end();
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const { Server } = require("socket.io");
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Socket.IO server is running");
+});
+
+// Initialize Socket.IO
+const io = new Server(server);
+
+// Define WebSocket connection event
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle WebSocket events here
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    // Broadcast the message to all connected clients
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+// Start the server
+server.listen(3001, () => {
+  console.log("Socket.IO server listening on *:3001");
+});
