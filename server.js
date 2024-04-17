138245a9da4fe6e03a52fbad702a1c112459fa34
Diome Nike <diomenikep@gmail.com>
2024-04-17 20:40:07 +0800

    test

--- server.js
+++ server.js
@@ -1,64 +1,41 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
-const { Server } = require("socket.io");
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
+const path = require("path");
 
-const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
+const app = express();
+const server = http.createServer(app);
 
-// Create a Next.js app
-const app = next({ dev });
-const handle = app.getRequestHandler();
+// Enable CORS for all origins in Express app
+app.use(cors());
 
-// Prepare the Next.js app
-app.prepare().then(() => {
-  // Create an HTTP server
-  const httpServer = createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
+// Serve static assets from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
-      if (pathname === "/a") {
-        await app.render(req, res, "/a", query);
-      } else if (pathname === "/b") {
-        await app.render(req, res, "/b", query);
-      } else {
-        await handle(req, res, parsedUrl);
-      }
-    } catch (err) {
-      console.error("Error occurred handling", req.url, err);
-      res.statusCode = 500;
-      res.end("internal server error");
-    }
+const io = socketIo(server, {
+  cors: {
+    origin: `http://localhost:3001`,
+    methods: ["GET", "POST"],
+  },
+});
+// Define WebSocket connection event
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Handle WebSocket 'message' event
+  socket.on("message", (msg) => {
+    console.log("message: " + msg);
+    // Broadcast the message to all connected clients
+    io.emit("message", msg);
   });
 
-  // Create a WebSocket server
-  const io = new Server(httpServer, {
-    /* options */
+  // Handle WebSocket 'disconnect' event
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
   });
+});
 
-  // Define WebSocket connection event
-  io.on("connection", (socket) => {
-    console.log("A user connected");
-
-    // Handle WebSocket 'message' event
-    socket.on("message", (msg) => {
-      console.log("message: " + msg);
-      // Broadcast the message to all connected clients
-      io.emit("message", msg);
-    });
-
-    // Handle WebSocket 'disconnect' event
-    socket.on("disconnect", () => {
-      console.log("User disconnected");
-    });
-  });
-
-  // Start the HTTP server
-  httpServer.listen(port, () => {
-    console.log(`> Ready on http://${hostname}:${port}`);
-  });
+server.listen(3000, () => {
+  console.log(`Server is running on port http://localhost:3000`);
 });
